c6d030b3204dd7988110c9790de1797c56c40de2
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-05-17 23:10:07 +0100

    index.js modified

--- models/index.js
+++ models/index.js
@@ -1,4 +1,23 @@
-const { Sequelize } = require('sequelize');
+const {
+  Sequelize,
+  DataTypes,
+} = require('sequelize');
 const dbConfig = require('../config/dbConfig');
 
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD);
+const sequelize = new Sequelize(
+  dbConfig.DB,
+  dbConfig.USER,
+  dbConfig.PASSWORD,
+  {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
+    operatorsAliases: false,
+
+    pool: {
+      max: dbConfig.pool.max,
+      min: dbConfig.pool.min,
+      acquire: dbConfig.pool.acquire,
+      idle: dbConfig.pool.idle,
+    },
+  }
+);
