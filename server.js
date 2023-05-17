af85df346bea53617b7f214fcb95cdea08cde2ed
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-05-17 22:48:07 +0100

    server.js implemented and dbConfig modified

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
 var corOptions = {
-  origin: "https://localhost:8081",
+  origin: 'https://localhost:8081',
 };
 
 //middleware
@@ -13,10 +13,20 @@ app.use(cors(corOptions));
 
 app.use(express.json());
 
-app.use(express.urlEncoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 //testing api
 
-app.get("/", (req, res) => {
-  res.json({ message: "hello from api" });
+app.get('/', (req, res) => {
+  res.json({ message: 'hello from api' });
+});
+
+//port
+
+const PORT = process.env.PORT || 8080;
+
+//server
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
