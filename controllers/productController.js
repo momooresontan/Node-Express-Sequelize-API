d7830ca3702dd911844026fa231dfb70e8453bf7
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-05-19 06:54:51 +0100

    get all function created in product controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -13,5 +13,22 @@ const addProduct = async (req, res) => {
     title: req.body.title,
     price: req.body.price,
     description: req.body.description,
+    published: req.body.published
+      ? req.body.published
+      : false,
   };
+
+  const product = await Product.create(info);
+  res.status(200).send(product);
+
+  console.log(product);
+};
+
+//2) Get all products
+
+const getAllProducts = async (req, res) => {
+  let products = await Product.findAll({
+    //attributes: ['title', 'price'],
+  });
+  res.status(200).send(products);
 };
