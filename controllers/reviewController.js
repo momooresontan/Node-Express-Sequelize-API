9f4c0bb91f767bcfd3130c8d9c09f7a2e7c47d94
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-05-27 04:42:27 +0100

    review controller modified(gat all, get one, update function)

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -17,5 +17,24 @@ exports.addReview = async (req, res) => {
 
 // 2) Get All Reviews
 exports.getAllReviews = async (req, res) => {
-  const reviews = await Review.findAll();
+  const reviews = await Review.findAll({});
+  res.status(200).send(reviews);
+};
+
+// 3) Get One Review
+exports.getOneReview = async (req, res) => {
+  const id = req.params.id;
+  const review = await Review.findOne({
+    where: { id },
+  });
+  res.status(200).send(review);
+};
+
+// 4) Update Review
+exports.updateReview = async (req, res) => {
+  const id = req.params.id;
+  const review = await Review.update(req.body, {
+    where: { id },
+  });
+  res.status(200).send(review);
 };
