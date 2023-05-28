e6d10829f3f767690bcd36332ecb04d7939b23a1
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-05-28 02:59:46 +0100

    index.js modified(1:M relation)

--- models/index.js
+++ models/index.js
@@ -1,26 +1,18 @@
-const {
-  Sequelize,
-  DataTypes,
-} = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 const dbConfig = require('../config/dbConfig');
 
-const sequelize = new Sequelize(
-  dbConfig.DB,
-  dbConfig.USER,
-  dbConfig.PASSWORD,
-  {
-    host: dbConfig.HOST,
-    dialect: dbConfig.dialect,
-    operatorsAliases: false,
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  operatorsAliases: false,
 
-    pool: {
-      max: dbConfig.pool.max,
-      min: dbConfig.pool.min,
-      acquire: dbConfig.pool.acquire,
-      idle: dbConfig.pool.idle,
-    },
-  }
-);
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle,
+  },
+});
 
 sequelize
   .authenticate()
@@ -36,17 +28,22 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.products = require('./productModel')(
-  sequelize,
-  DataTypes
-);
-db.reviews = require('./reviewModel')(
-  sequelize,
-  DataTypes
-);
+db.products = require('./productModel')(sequelize, DataTypes);
+db.reviews = require('./reviewModel')(sequelize, DataTypes);
 
 db.sequelize.sync({ force: false }).then(() => {
   console.log('yes re-sync done!');
+});
+
+// 1:M Relation
+db.products.hasMany(db.reviews, {
+  foreignKey: 'product_id',
+  as: 'review',
+});
+
+db.reviews.belongsTo(db.products, {
+  foreignKey: 'product_id',
+  as: 'product',
 });
 
 module.exports = db;
